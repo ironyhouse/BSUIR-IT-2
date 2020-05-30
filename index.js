5e81635dd6a148fd182d90dd683663541271906b
ironyhouse <ironylait@gmail.com>
2020-05-30 19:03:48 +0300

    init: load project

---
+++ index.js
@@ -0,0 +1,45 @@
+function SHA () {
+    this.encode = function (inputText) {
+        const text = inputText;
+
+        async function digestMessage(message) {
+            const msgUint8 = new TextEncoder().encode(message);                           // кодировка (utf-8)
+            const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);           // хешировать сообщение
+            const hashArray = Array.from(new Uint8Array(hashBuffer));                     // преобразовать в байтовый массив
+            const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // преобразовать байты в шестнадцатеричную строку
+            return hashHex;
+        }
+
+        (async function () {
+            let digestHex_256 = await digestMessage(text);
+            digestHex = digestHex_256;
+        })();
+
+        return digestHex;
+    };
+}
+
+
+//Вызов функции
+document.querySelector('#encodeButton').addEventListener('click', () => {
+    textOut.value = new SHA().encode(inputText.value);
+});
+
+
+//Чтение из файла
+function readFileEncode(input) {
+    let file = input.files[0];
+
+    let reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function () {
+        inputText.value = reader.result;
+    };
+
+    reader.onerror = function () {
+        inputText.value = reader.error;
+    };
+}
+
